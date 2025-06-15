84804589ef39b072c6c40b3b7349e8c09cc237bf
immaninder <manindersinghofficial235@gmail.com>
2025-06-15 15:00:27 +0530

    update

---
+++ python3.py
@@ -0,0 +1,131 @@
+#question 1
+'''
+n=int(input("number of month"))
+if(n==1):
+    print("january")
+elif(n==2):
+    print("february")
+elif(n==3):
+    print("march")
+elif(n==4):
+    print("april")
+elif(n==5):
+    print("may")
+elif(n==6):
+    print("june")
+elif(n==7):
+    print("july")
+elif(n==8):
+    print("august")
+elif(n==9):
+    print("september")
+elif(n==10):
+    print("october")
+elif(n==11):
+    print("november")
+elif(n==12):
+    print("december")
+else:
+    print("invalid input")
+'''
+'''
+#question2
+a=int(input("first number"))
+b=int(input("second number"))
+if a==b:
+    print("both are equal")
+else:
+    print(" both are  not equal")
+if a>b:
+    print("a is bigger than b")
+else:
+    print("b is bigger than a")
+if a<b:
+    print("a is smaller than b")
+elif a==b:
+    print("a is equal to b")
+c=input("first name") 
+d=input("surname")
+e=len(c)
+f=len(d)
+if a>b:
+    for i in range(0,5):
+      print(c)
+else:
+    for i in range(0,3):
+        print(d)
+'''
+
+
+
+
+
+
+
+
+'''
+#question3
+a=input("first string")
+b=input("input second string")
+if a==b:
+    print("Both strings are equal")
+else:
+    print("Both strings are not equal")
+if a is b:
+    print("both strings are equal")
+else:
+    print("both strings are not equal")
+'''
+'''
+#question5
+a=int(input("enter a"))
+b=0
+for i in range(0,a+1)
+   b+=i
+print("sum upto a is",b)
+'''
+
+#question6
+'''
+a=int(input("enter a number"))
+for i in range(0,a):
+    if(i%2==0):
+        print(i)
+'''
+'''
+#question 8
+h=int(input("enter h"))
+print("table of h",h)
+for i in range(0,11):
+    print(h*i)
+'''
+
+#question9
+'''
+for i in range(0,4):
+    for j in range(0,i+1):
+        print(j+1,' ',end="")
+print()
+'''
+'''
+#question10
+n=int(input("enter n"))
+for i in range(1,n+1):
+    print(i**2)
+'''
+'''
+#question7
+n=int(input("enter a number"))
+a=input("enter+,-")
+if(a=="+"):
+    for i in range(0,n+1):
+        print(i)
+elif:(a=="-"):
+    for i in range(n+1,-1,-1)
+    print(i)
+'''
+
+#question4
+#Error identified 
+
+print("change by maninder")
